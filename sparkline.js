ba43b8ef91becf61efdc4335b074eb43b93c99e9
Chris Webb <chris@code-in-python.com>
2020-04-01 14:18:26 +0100

    Source code for http://www.codedrome.com/sparklines-in-javascript/

---
+++ sparkline.js
@@ -0,0 +1,41 @@
+﻿
+function createSparkline(data, spacing, height, color, elementid)
+{
+	const min = Math.min(...data);
+	const max = Math.max(...data);
+	const range = max - min;
+	const pixelsperunit = height / range;
+	let x = 0;
+	let y1 = 0;
+	let y2 = 0;
+
+	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+    svg.setAttributeNS(null, "width", spacing * (data.length - 1));
+    svg.setAttributeNS(null, "height", height);
+
+	for(let i = 0, l = data.length - 1; i < l; i++)
+	{
+		y1 = height - ((data[i] - min) * pixelsperunit);
+		y2 = height - ((data[i+1] - min) * pixelsperunit);
+		drawLine(svg, x, y1, x + spacing, y2, color);
+		x += spacing;
+	}
+
+	document.getElementById(elementid).appendChild(svg);
+}
+
+function drawLine(element, x1, y1, x2, y2, color)
+{
+	const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+	line.setAttributeNS(null, "x1", x1);
+	line.setAttributeNS(null, "y1", y1);
+	line.setAttributeNS(null, "x2", x2);
+	line.setAttributeNS(null, "y2", y2);
+
+	line.setAttributeNS(null, "stroke", color);
+	line.setAttributeNS(null, "stroke-width", 1);
+
+	element.appendChild(line);
+}
---
+++ sparklinedemo.js
@@ -0,0 +1,23 @@
+﻿
+window.onload = function()
+{
+    const prices = getPrices();
+
+    for(price of prices)
+    {
+        createSparkline(price.prices, 16, 16, "#FF0000", price.code);
+    }
+}
+
+
+function getPrices()
+{
+    const prices = [{code: "gs", prices: [401.5, 443.9, 422.2, 432.7, 483.1, 501.8, 538.4, 522.7]},
+                    {code: "nm", prices: [187.5, 162.9, 151.0, 188.1, 224.4, 218.9, 201.5, 192.6]},
+                    {code: "ca", prices: [855.3, 841.0, 801.5, 818.9, 832.3, 850.7, 880.6, 895.2]},
+                    {code: "os", prices: [255.5, 285.9, 310.8, 322.4, 305.1, 276.7, 260.0, 292.4]},
+                    {code: "ta", prices: [390.4, 485.9, 466.7, 380.2, 360.7, 420.8, 441.4, 475.9]},
+                    {code: "xs", prices: [62.4, 52.9, 44.2, 64.8, 74.0, 75.2, 95.5, 102.7]}];
+
+    return prices;
+}
